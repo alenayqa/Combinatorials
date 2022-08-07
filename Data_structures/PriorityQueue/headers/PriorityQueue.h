51c50748e49f0b5a8134cad080234c8290e29a4e
alenayqa <andr.sherst@gmail.com>
2022-08-07 21:23:22 +0500

    priority queue: constructors

--- Data_structures/PriorityQueue/headers/PriorityQueue.h
+++ Data_structures/PriorityQueue/headers/PriorityQueue.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <algorithm>
+#include "Vertex.h"
 
 using namespace std;
 
@@ -7,10 +9,12 @@ template<typename T>
 class PriorityQueue
 {
 private:
-
+    int volume;
+    int _size;
+    Vertex<T>* arr;
 public:
 
-    PriorityQueue();
+    PriorityQueue(int _volume);
     ~PriorityQueue();
 
     bool add(T _value, int _priority);
@@ -19,3 +23,17 @@ public:
     T* get_minimal();
 
 };
+
+template <typename T>
+PriorityQueue<T>::PriorityQueue(int _volume = 5)
+{
+    volume = _volume;
+    _size = 0;
+    arr = new Vertex<T>[_volume];
+}
+
+template <typename T>
+PriorityQueue<T>::~PriorityQueue()
+{
+    delete[] arr;
+}
---
+++ Data_structures/PriorityQueue/headers/Vertex.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+struct Vertex
+{
+    T data;
+    int priority
+
+    Vertex(T _data, int _priority)
+    {
+        data = _data;
+        priority = _priority;
+    }
+};
