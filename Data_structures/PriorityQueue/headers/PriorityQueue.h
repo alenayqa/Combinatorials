0a0f310e177680ca71b63a29d441a016e659512a
alenayqa <andr.sherst@gmail.com>
2022-08-09 00:14:41 +0500

    Preparation for insertions

--- Data_structures/PriorityQueue/headers/PriorityQueue.h
+++ Data_structures/PriorityQueue/headers/PriorityQueue.h
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include "Vertex.h"
 
+// https://dementiy.gitbooks.io/algo/content/heaps.html
+
 using namespace std;
 
 template<typename T>
@@ -11,7 +13,14 @@ class PriorityQueue
 private:
     int volume;
     int _size;
-    Vertex<T>* arr;
+    Vertex<T>** arr;
+
+    void swap(int ind1, int ind2);
+
+    int parent(int i) { return (i-1) / 2;}
+    int left(int i) { return 2*i + 1;}
+    int right(int i) { return 2*i + 2;}
+
 public:
 
     PriorityQueue(int _volume);
@@ -25,15 +34,32 @@ public:
 };
 
 template <typename T>
+T* PriorityQueue<T>::get_minimal()
+{
+    if (arr[0])
+        return &arr[0].data;
+    else return nullptr;
+}
+
+template <typename T>
 PriorityQueue<T>::PriorityQueue(int _volume = 5)
 {
     volume = _volume;
     _size = 0;
-    arr = new Vertex<T>[_volume];
+    arr = new Vertex<T>*[_volume];
 }
 
 template <typename T>
 PriorityQueue<T>::~PriorityQueue()
 {
+    for (int i = 0; i<volume; i++)
+        delete arr[i];
     delete[] arr;
+}
+template <typename T>
+void PriorityQueue<T>::swap(int ind1, int ind2)
+{
+    T* tmp = arr[ind1];
+    arr[ind1] = arr[ind2];
+    arr[ind2] = tmp;
 }
