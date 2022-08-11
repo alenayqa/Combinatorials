a7a69b31ddcce47ed292ecc608e75748ea220cb8
sad <asherstobitov@lab-code.com>
2022-08-11 15:18:39 +0500

    Vertex for the tree

---
+++ Data_structures/AATree/headers/Vertex.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+
+template <typename T>
+class Vertex
+{
+public:
+    T data;
+    int level;
+
+    Vertex<T>* l = nullptr;
+    Vertex<T>* r = nullptr;
+
+    Vertex<T>(T _data, int _level, Vertex<T>* _l, Vertex<T>* _r);
+    ~Vertex<T>();
+};
+
+Vertex<T>::Vertex(T _data, int _level, Vertex<T>* _l, Vertex<T>* _r)
+{
+    l = _l;
+    r = _r;
+    level = _level;
+    data = _data;
+}
+
+Vertex<T>::~Vertex()
+{
+    delete l; delete r;
+}
