ee0bba05003fa95bcae0ece0fb46cc99db4b519b
alenayqa <andr.sherst@gmail.com>
2022-08-27 18:00:14 +0500

    Add methods descriptors

--- Data_structures/AATree/headers/AATree.h
+++ Data_structures/AATree/headers/AATree.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include "Vertex.h"
 
 using namespace std;
 
@@ -7,5 +8,18 @@ template <typename K, typename D>
 class AATree
 {
 private:
+    Vertex<K, D>* root = nullptr;
 public:
+    void add(K _key, D _data);
+    void remove(K _key);
+    
+    D* find(K _key);
+
+    ~AATree<K,D>();
 };
+
+template <typename K, typename D>
+AATree<K, D>::~AATree()
+{
+    if (root) delete root;
+}
