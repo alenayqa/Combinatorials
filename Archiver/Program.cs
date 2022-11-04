c9acb678dcebbba862b391aef2f4cf16ec931e5a
alenayqa <andr.sherst@gmail.com>
2022-11-04 23:16:26 +0500

    Switch to c#

---
+++ Archiver/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Archiver
+{
+    class Program 
+    {         
+        static void Main(string[] args)
+        {
+            string s = "ПРИВЕТ!";
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine(s[i]);
+            }
+        }
+    }
+}
